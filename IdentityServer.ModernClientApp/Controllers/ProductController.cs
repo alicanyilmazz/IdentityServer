a2217543aaabb8a6d10b9e5cdeaba22d88252828
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:50:13 +0300

    ProductController created.

---
+++ IdentityServer.ModernClientApp/Controllers/ProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.API1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+
+    }
+}
