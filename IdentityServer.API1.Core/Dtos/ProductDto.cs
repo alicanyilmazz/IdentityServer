e58c3781d5255afe837b8f0bf96573297eec8e44
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:41:17 +0300

    ProductDto created.

---
+++ IdentityServer.API1.Core/Dtos/ProductDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API1.Core.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
