2b16f2b89fdc101d5622dcd1e729947dc050af80
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:46:16 +0300

    MovieConfiguration created.

---
+++ IdentityServer.API2.Data/Configurations/EntityConfigurations/MovieConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer.API2.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API2.Data.Configurations.EntityConfigurations
+{
+    internal class MovieConfiguration : IEntityTypeConfiguration<Movie>
+    {
+        public void Configure(EntityTypeBuilder<Movie> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.ReleaseDate).HasDefaultValue(DateTime.UtcNow);
+        }
+    }
+}
