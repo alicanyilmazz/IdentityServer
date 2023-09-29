da2912323fca02b061af14ce413eeb0c839afee0
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:46:49 +0300

    UnitOfWork created.

---
+++ IdentityServer.API2.Data/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,29 @@
+﻿using IdentityServer.API2.Core.UnitOfWork;
+using IdentityServer.API2.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API2.Data.UnitOfWork
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext _context;
+        public UnitOfWork(AppDbContext dbContext)
+        {
+            _context = dbContext;
+        }
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
