01340f348e019acccb637ca0e5719be06a63f3cf
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:45:14 +0300

    Movie created.

---
+++ IdentityServer.API2.Core/Entities/Movie.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API2.Core.Entities
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
