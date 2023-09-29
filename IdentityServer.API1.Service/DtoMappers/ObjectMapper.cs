181f10d2b1d083e6534c84916836a454f12115a2
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:44:31 +0300

    ObjectMapper created.

---
+++ IdentityServer.API1.Service/DtoMappers/ObjectMapper.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using IdentityServer.API1.Service.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API1.Service.DtoMappers
+{
+    public class ObjectMapper
+    {
+        private static readonly Lazy<IMapper> lazy = new Lazy<IMapper>(() =>
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DtoMapper>();
+            });
+            return config.CreateMapper();
+        });
+
+        public static IMapper Mapper => lazy.Value;
+    }
+}
