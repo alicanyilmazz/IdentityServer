fe540c931fd2088607af9a3bc9f31df56b586ab2
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:46:27 +0300

    AppDbContext created.

---
+++ IdentityServer.API2.Data/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using IdentityServer.API2.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API2.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
