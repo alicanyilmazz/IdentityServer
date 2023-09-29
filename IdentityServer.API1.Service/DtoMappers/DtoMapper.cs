35a9bb7ef47efff8d12965cd46f1d9f1f699f4c3
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:44:21 +0300

    DtoMapper created.

---
+++ IdentityServer.API1.Service/DtoMappers/DtoMapper.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using IdentityServer.API1.Core.Dtos;
+using IdentityServer.API1.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API1.Service.DtoMappers
+{
+    public class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<Product, ProductDto>().ReverseMap();
+        }
+    }
+}
