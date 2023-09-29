9f185fa16b3207d80924bec65df90d4b36c6ddf5
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:50:54 +0300

    IdentityServer added.

--- IdentityServer.ModernClientApp/Program.cs
+++ IdentityServer.ModernClientApp/Program.cs
@@ -1,3 +1,6 @@
+using IdentityServer.SharedLibrary.Extensions.Authorization;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.AddCustomAuthentication("https://localhost:7025", "resource_api1");
+//builder.Services.AddAuthentication(options =>
+//{
+//    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+//}).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme,option =>
+//{
+//    option.Authority = "https://localhost:7025";
+//    option.Audience = "resource_api1";
+//});
 
 var app = builder.Build();
 
@@ -17,7 +29,7 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
