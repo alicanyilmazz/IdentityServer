ca1c0f7f644f2e42b3024a5e97e94d4fe60a2b8b
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:52:51 +0300

    CustomExceptionHandler created.

---
+++ IdentityServer.SharedLibrary/Extensions/Exception/CustomExceptionHandler.cs
@@ -0,0 +1,46 @@
+﻿using IdentityServer.SharedLibrary.Dtos;
+using IdentityServer.SharedLibrary.Exceptions;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace IdentityServer.SharedLibrary.Extensions.Exception
+{
+    public static class CustomExceptionHandler
+    {
+        public static void UseCustomException(this IApplicationBuilder app)
+        {
+            app.UseExceptionHandler(config =>
+            {
+                config.Run(async context =>
+                {
+                    context.Response.StatusCode = 500;
+                    context.Response.ContentType = "application/json";
+                    var error = context.Features.Get<IExceptionHandlerFeature>();
+                    if (error != null)
+                    {
+                        var ex = error.Error;
+                        ErrorDto? errorDto = null;
+
+                        if (ex is CustomException)
+                        {
+                            errorDto = new ErrorDto(ex.Message, true);
+                        }
+                        else
+                        {
+                            errorDto = new ErrorDto(ex.Message, false);
+                        }
+                        var response = Response<NoDataDto>.Fail(errorDto, 500);
+                        await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                    }
+                });
+            });
+        }
+    }
+}
