08d98c0a9d42cda8c181f7ca9dcc696aad9f0a76
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:45:06 +0300

    MovieDto created.

---
+++ IdentityServer.API2.Core/Dtos/MovieDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API2.Core.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public DateTime ReleaseDate { get; set; }
+    }
+}
