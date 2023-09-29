87319413badc0e8c6468bec48f27b5c2e491780c
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:52:39 +0300

    AuthorizationManager created.

---
+++ IdentityServer.SharedLibrary/Extensions/Authorization/AuthorizationManager.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.SharedLibrary.Extensions.Authorization
+{
+    public static class AuthorizationManager
+    {
+        public static void AddCustomAuthentication(this WebApplicationBuilder builder , string authority , string audience)
+        {
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, option =>
+            {
+                option.Authority = authority;
+                option.Audience = audience;
+            });
+        }
+    }
+}
