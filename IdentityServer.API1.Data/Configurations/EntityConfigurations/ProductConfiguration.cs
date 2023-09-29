82f9a450ab1503df2c92bd38ffa5dd636093993a
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:43:18 +0300

    ProductConfiguration added.

---
+++ IdentityServer.API1.Data/Configurations/EntityConfigurations/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using IdentityServer.API1.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API1.Data.Configurations.EntityConfigurations
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired();
+            builder.Property(x => x.Price).HasDefaultValue(0);
+        }
+    }
+}
