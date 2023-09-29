9915fa40649b992b99185f284d08125b7a4c6983
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-29 21:43:28 +0300

    AppDbContext added.

---
+++ IdentityServer.API1.Data/Context/AppDbContext.cs
@@ -0,0 +1,26 @@
+﻿using IdentityServer.API1.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.API1.Data.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
