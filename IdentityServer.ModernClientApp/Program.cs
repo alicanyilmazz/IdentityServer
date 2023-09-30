21b72222474132c025b7604f8a232e27c1251c66
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-30 15:36:44 +0300

    EFCore Service added.

--- IdentityServer.ModernClientApp/Program.cs
+++ IdentityServer.ModernClientApp/Program.cs
@@ -1,5 +1,10 @@
+using IdentityServer.API1.Data.Context;
+using IdentityServer.SharedLibrary.Common.Constants.InitializeSettings;
+using IdentityServer.SharedLibrary.Configuration.TokenConfigurations;
 using IdentityServer.SharedLibrary.Extensions.Authorization;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +14,15 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.AddCustomAuthentication("https://localhost:7025", "resource_api1");
+builder.Services.AddDbContext<AppDbContext>(x =>
+{
+    x.UseSqlServer(builder.Configuration.GetConnectionString(InitializeSetting.SQL_CONFIGURATION), option =>
+    {
+        option.MigrationsAssembly(Assembly.GetAssembly(typeof(AppDbContext)).GetName().Name); 
+    });
+});
+builder.Services.Configure<TokenConfiguration>(builder.Configuration.GetSection(InitializeSetting.TOKEN_CONFIGURATIONS));
+builder.AddCustomAuthentication(tokenConfiguration: builder.Configuration.GetSection(InitializeSetting.TOKEN_CONFIGURATIONS).Get<TokenConfiguration>());
 //builder.Services.AddAuthentication(options =>
 //{
 //    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
