e8d12fb4a23528712e6ff66618da4ea5a54e3bc6
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-30 15:35:57 +0300

    TokenConfiguration created.

---
+++ IdentityServer.SharedLibrary/Configuration/TokenConfigurations/TokenConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.SharedLibrary.Configuration.TokenConfigurations
+{
+    public class TokenConfiguration
+    {
+        public List<string>? Audience { get; set; }
+        public string? Authority { get; set; }
+    }
+}
