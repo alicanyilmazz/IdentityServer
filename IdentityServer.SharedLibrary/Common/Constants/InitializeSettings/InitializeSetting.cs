f530004bdd9da7b84e471c6dbf4dcba4efce74fc
alican yılmaz <alicanyilmaz101@gmail.com>
2023-09-30 15:35:45 +0300

    Initialize settings created.

---
+++ IdentityServer.SharedLibrary/Common/Constants/InitializeSettings/InitializeSetting.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IdentityServer.SharedLibrary.Common.Constants.InitializeSettings
+{
+    public class InitializeSetting
+    {
+        public const string TOKEN_CONFIGURATIONS = "TokenConfigurations";
+        public const string SQL_CONFIGURATION = "SqlServer";
+    }
+}
