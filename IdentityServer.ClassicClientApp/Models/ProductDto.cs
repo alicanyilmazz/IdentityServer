67412c652c764e275da2897193438d29664f024e
alican yılmaz <alicanyilmaz101@gmail.com>
2023-10-09 00:19:01 +0300

    ProductDto created for MVC.

---
+++ IdentityServer.ClassicClientApp/Models/ProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace IdentityServer.Client1.Models
+{
+    public class ProductDto
+    {
+        public List<Product> Data { get; set; }
+        public int StatusCode { get; set; }
+        public string? Error { get; set; }
+    }
+    public class Product
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
