b8a429e5559ac978d0c42606dc06a37a025f51da
alican yılmaz <alicanyilmaz101@gmail.com>
2023-11-19 23:02:13 +0300

    Client3 Application created.

---
+++ IdentityServer.Client3/Program.cs
@@ -0,0 +1,32 @@
+using IdentityServer.SharedLibrary.Common.Constants.InitializeSettings;
+using IdentityServer.SharedLibrary.Configuration.CookieConfigurations;
+using IdentityServer.SharedLibrary.Extensions.Authorization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+builder.Services.Configure<CookieConfiguration>(builder.Configuration.GetSection(InitializeSetting.COOKIE_CONFIGURATIONS));
+builder.AddCustomCookieAuthentication(cookieConfiguration: builder.Configuration.GetSection(InitializeSetting.COOKIE_CONFIGURATIONS).Get<CookieConfiguration>());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+app.Run();
