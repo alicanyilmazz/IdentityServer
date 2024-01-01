962c443ee481a3ab984cf7dc210ced753779d686
alican yılmaz <alicanyilmaz101@gmail.com>
2024-01-01 17:56:33 -0500

    User controller created.

---
+++ IdentityServer.Client3/Controllers/UserController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Client3.Controllers
+{
+    [Authorize]
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
