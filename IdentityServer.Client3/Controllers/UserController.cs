06e2809a3cf1f31ebd7feec7111b330c64892cbe
alican yılmaz <alicanyilmaz101@gmail.com>
2024-01-21 00:59:39 -0500

    User Controller modified for User Claims and Authentication Properties.

--- IdentityServer.Client3/Controllers/UserController.cs
+++ IdentityServer.Client3/Controllers/UserController.cs
@@ -1,13 +1,33 @@
-﻿using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
 
 namespace IdentityServer.Client3.Controllers
 {
     [Authorize]
     public class UserController : Controller
     {
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
+            var authenticationValues = await HttpContext.AuthenticateAsync();
+            if (authenticationValues != null)
+            {
+                var properties = authenticationValues.Properties.Items;
+                foreach (var property in properties)
+                {
+                    Debug.WriteLine($"{property.Key} - {property.Value}");
+                }
+            }
+            foreach (var claim in User.Claims)
+            {
+                Debug.WriteLine($"{claim.Type} - {claim.Value}");
+            }
+            var authenticatedUserClaim = User.Claims.Where(x=>x.Type == ClaimTypes.NameIdentifier).FirstOrDefault();
+            var authenticatedUserCountry = User.Claims.Where(x => x.Type == ClaimTypes.Country).FirstOrDefault();
+            var authenticatedUserId = authenticatedUserClaim.Value;
+            Debug.WriteLine($"{authenticatedUserId}");
             return View();
         }
     }
