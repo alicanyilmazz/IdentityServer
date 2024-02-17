31f47c5002f0623ad108569a1a6f090f38396bb3
alican yılmaz <alicanyilmaz101@gmail.com>
2024-02-17 16:32:27 -0500

    ErrorController created for invalid request

---
+++ IdentityServer.Client3/Controllers/ErrorController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace IdentityServer.Client3.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.ErrorMessage = "Üzgünüz, istediğiniz sayfa bulunamadı.";
+                    break;
+            }
+            return View("NotFound");
+        }
+    }
+}
