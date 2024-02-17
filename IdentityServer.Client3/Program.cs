78811c43d22bf25aa9fe95f4a4213b74f187041b
alican yılmaz <alicanyilmaz101@gmail.com>
2024-02-17 16:36:33 -0500

    Config changed for NotFoundPage.

--- IdentityServer.Client3/Program.cs
+++ IdentityServer.Client3/Program.cs
@@ -15,10 +15,16 @@ builder.AddCustomCookieAuthentication(cookieConfiguration: builder.Configuration
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+//if (!app.Environment.IsDevelopment())
+//{
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+if (app.Environment.IsDevelopment())
 {
+    app.UseStatusCodePagesWithReExecute("/Error/{0}");
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
