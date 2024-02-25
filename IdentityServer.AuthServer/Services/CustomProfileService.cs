839070ec64a5ad304ffcc035b7b5491036244073
alican yılmaz <alicanyilmaz101@gmail.com>
2024-02-25 07:39:08 -0500

    CustomProfileService created.

---
+++ IdentityServer.AuthServer/Services/CustomProfileService.cs
@@ -0,0 +1,48 @@
+﻿using IdentityServer.AuthServer.Repository;
+using IdentityServer4.Extensions;
+using IdentityServer4.Models;
+using IdentityServer4.Services;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+
+namespace IdentityServer.AuthServer.Services
+{
+    public class CustomProfileService : IProfileService
+    {
+        private readonly ICustomUserRepository _customUserRepository;
+
+        public CustomProfileService(ICustomUserRepository customUserRepository)
+        {
+            _customUserRepository = customUserRepository;
+        }
+
+        public async Task GetProfileDataAsync(ProfileDataRequestContext context)
+        {
+            var userId = context.Subject.GetSubjectId();
+            var user = await _customUserRepository.FindById(int.Parse(userId));
+            var claims = new List<Claim>()
+            {
+                new Claim(JwtRegisteredClaimNames.Email,user.Email),
+                new Claim("name",user.Username),
+            };
+
+            if (user.Id == 1)
+            {
+                claims.Add(new Claim("role", "admin"));
+            }
+            else
+            {
+                claims.Add(new Claim("role", "customer"));
+            }
+            context.AddRequestedClaims(claims);
+            context.IssuedClaims = claims; //Not Recommended.// You can Use UserInfoEndpoint
+        }
+
+        public async Task IsActiveAsync(IsActiveContext context)
+        {
+            var userId = context.Subject.GetSubjectId();
+            var user = await _customUserRepository.FindById(int.Parse(userId));
+            context.IsActive = user != null;
+        }
+    }
+}
