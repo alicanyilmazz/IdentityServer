1f4fecd058af0c4335cd0c1dfd584fff617ad9ab
alican yılmaz <alicanyilmaz101@gmail.com>
2024-02-25 07:40:51 -0500

    CustomUserRepository and ICustomUserRepository created.

---
+++ IdentityServer.AuthServer/Repository/CustomUserRepository.cs
@@ -0,0 +1,31 @@
+﻿using IdentityServer.AuthServer.Models;
+using Microsoft.EntityFrameworkCore;
+using static IdentityServer4.Models.IdentityResources;
+
+namespace IdentityServer.AuthServer.Repository
+{
+    public class CustomUserRepository : ICustomUserRepository
+    {
+        private readonly CustomDbContext _customDbContext;
+
+        public CustomUserRepository(CustomDbContext customDbContext)
+        {
+            _customDbContext = customDbContext;
+        }
+
+        public async Task<CustomUser> FindByEmail(string email)
+        {
+            return await _customDbContext.CustomUsers.FirstOrDefaultAsync(x => x.Email == email);
+        }
+
+        public async Task<CustomUser> FindById(int id)
+        {
+            return await _customDbContext.CustomUsers.FindAsync(id);
+        }
+
+        public async Task<bool> Validate(string email, string password)
+        {
+            return await _customDbContext.CustomUsers.AnyAsync(x=>x.Email == email && x.Password == password);
+        }
+    }
+}
---
+++ IdentityServer.AuthServer/Repository/ICustomUserRepository.cs
@@ -0,0 +1,11 @@
+﻿using IdentityServer.AuthServer.Models;
+
+namespace IdentityServer.AuthServer.Repository
+{
+    public interface ICustomUserRepository
+    {
+        Task<bool> Validate(string email, string password);
+        Task<CustomUser> FindById(int id);
+        Task<CustomUser> FindByEmail(string email);
+    }
+}
